e53c4c7f8259f7fdb0dd1f9738f1f968335a6db1
haykuhi11-ar <harutyunyanhaykuhi4@gmail.com>
2025-10-14 11:27:35 +0400

    plusOne

---
+++ 08.10/plusOne.js
@@ -0,0 +1,14 @@
+var plusOne = function(digits) {
+for (let i = digits.length - 1; i >= 0; i--) {
+  if (digits[i] < 9) {
+    digits[i]++;
+    return digits;
+  }
+  if (digits[i] === 9) { 
+    digits[i] = 0;
+  }
+}
+return [1, ...digits];
+}
+let digits = [9, 9, 3, 4];
+console.log(plusOne(digits));
