fca294454c625e600017a5a6b619e3d7972d12a7
haykuhi11-ar <harutyunyanhaykuhi4@gmail.com>
2025-10-18 12:20:48 +0400

    16.10

---
+++ 16.10/island_perimeter.js
@@ -0,0 +1,19 @@
+const grid = [
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0]
+];
+var islandPerimeter = function(grid) {
+    let perimeter = 0;
+for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === 1) {
+             perimeter += 4;
+        if (i > 0 && grid[i - 1][j] === 1) perimeter -= 2;
+        if (j > 0 && grid[i][j - 1]  === 1) perimeter -= 2;
+        }
+    }
+}
+return perimeter;
+};
